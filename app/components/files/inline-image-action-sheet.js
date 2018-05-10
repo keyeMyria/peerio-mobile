e65515ab5d2abfd3d091c6a33bc16a15ebcf7d45
Karim94 <karimchehab94@gmail.com>
2018-05-10 09:01:06 -0400

    chore: lint

--- app/components/files/inline-image-action-sheet.js
+++ app/components/files/inline-image-action-sheet.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { observer } from 'mobx-react/native';
-import { when } from 'mobx';
 import SafeComponent from '../shared/safe-component';
 import { tx } from '../utils/translator';
 import { fileState } from '../states';
